e755616fa026806ac2101ce06c2f718c5270a257
Carlos Hernandez <car2262541@maricopa.edu>
2021-12-08 20:05:16 -0700

    Experiments with multiple versions of Lambda done!

--- Lab/Collections/src/App.java
+++ Lab/Collections/src/App.java
@@ -1,6 +1,7 @@
 package Lab.Collections.src;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -20,5 +21,16 @@ public class App {
 
         //optimized code - runs on multiple processors
         personsDB.forEach(p -> p.getFn().toUpperCase()); 
+
+        personsDB.sort(new Comparator<Person>() {
+            @Override
+            public int compare(Person p1, Person p2) {
+                return p1.getPersonID().compareTo(p2.getPersonID());
+            }
+        });
+
+        personsDB.sort((p1, p2) -> {
+            return p1.getPersonID().compareTo(p2.getPersonID());
+        });
     }
 }
